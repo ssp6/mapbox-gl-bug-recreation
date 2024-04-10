ccad8cd4db309d48adde63438b6d437c25a8b30d
Sean Paterson <sean.paterson@brandwidth.com>
2024-04-10 10:19:35 +0100

    feat: set up minimum required for nav bar bug

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
 
 function App() {
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+
+  useEffect(() => {
+    if (map.current) return // initialize map only once
+    // @ts-expect-error Wrong
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/brandwidth/clsvrnpk9005k01qu2cii0zb1',
+      latitude: 51.5081,
+      longitude: -0.1248,
+    })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="map-container">
+      {/* Fake Nav */}
+      <div className="fake-navbar" />
+      <div className="map" ref={mapContainer} />
     </div>
-  );
+  )
 }
 
 export default App;
